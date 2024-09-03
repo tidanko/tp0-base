97073bcfbbd6bdd55bb686cb61dfd1ee251868ec
tdanko <tdanko@Ubuntu.myguest.virtualbox.org>
2024-09-03 10:01:50 +0200

    ejercicio 3 cambios

---
+++ docker-compose-script.py
@@ -0,0 +1,51 @@
+import yaml
+import sys
+
+def main():
+    if len(sys.argv) != 3 or not sys.argv[2].isnumeric() or int(sys.argv[2]) < 1 or len(sys.argv[1]) < 6 or sys.argv[1][len(sys.argv[1]) - 5:] != '.yaml':
+        raise ValueError("The parameters introduced are incorrect. Use: ./generar-compose.sh <file-name> <clients_amount>")
+
+
+    file_name = sys.argv[1]
+    clients_amount = int(sys.argv[2])
+
+    data = {
+        'name': 'tp0',
+        'services': {
+            'server': {
+                'container_name': 'server',
+                'image': 'server:latest',
+                'entrypoint': 'python3 /main.py',
+                'environment': ['PYTHONUNBUFFERED=1', 'LOGGING_LEVEL=DEBUG'],
+                'networks': ['testing_net'],
+            }    
+        },
+        'networks': {
+            'testing_net': {
+                'ipam': {
+                    'driver': 'default',
+                    'config': [{'subnet': '172.25.125.0/24'}]
+                }
+            }
+        },
+        'volumes': ['./server/config.ini:/config.ini'],
+        }
+
+    for i in range(1, clients_amount + 1):
+        new_client = {
+            'container_name': f'client{i}',
+            'image': 'client:latest',
+            'entrypoint': '/client',
+            'environment': [f'CLI_ID={i}', 'CLI_LOG_LEVEL=DEBUG'],
+            'networks': ['testing_net'],
+            'depends_on': ['server'],
+            'volumes': ['./client/config.yaml:/config.yaml']
+        }
+        data['services'][f'client{i}'] = new_client
+    
+    with open(file_name, 'w') as file:
+        yaml.dump(data, file, sort_keys=False)
+
+    return
+
+main()
--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,3 +1,3 @@
-docker build -t test .
-. ./test.env
-python3 validar-input.py "$MESSAGE" "$(docker run --network=tp0_testing_net -e MESSAGE="$MESSAGE" -e PORT=$PORT test)"
+docker build -t input_validating .
+. ./input_validating.env
+python3 validar-input.py "$MESSAGE" "$(docker run --network=tp0_testing_net -e MESSAGE="$MESSAGE" -e PORT=$PORT input_validating)"
